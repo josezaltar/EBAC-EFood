8ea58d7fa148ec5face6e4049d562cd0f9194588
zezaltar <josezaltar@gmail.com>
2025-06-06 13:37:35 -0300

    Desfazendo

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import Footer from './container/Footer'
 import EstiloGlobal from './styles'
 import Rotas from './routes'
 
-// Importando o pacote para ativar o Analytics automaticamente
-import '@vercel/analytics' // Importação simples do pacote
-
 function App() {
   return (
     <BrowserRouter>
